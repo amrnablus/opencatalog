2daa0ecd02cccdaf719ed7f8817c339ced32812d
ahmad moqanasa <amoqanasa@souq.com>
2014-04-04 19:02:02 +0300

    remove unused code

--- src/main/java/com/gmail/amrnablus/opencatalog/controller/ProductController.java
+++ src/main/java/com/gmail/amrnablus/opencatalog/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.gmail.amrnablus.opencatalog.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,24 +21,24 @@ public class ProductController {
 	private ProductService productService;
 
 	@RequestMapping(value = {"/product/list","/product","/"}, method = RequestMethod.GET)
-	public @ResponseBody List<Product> getProductsList(ModelMap model) {
+	public @ResponseBody List<Product> getProductsList() {
 		return productService.listProducts();
 	}
 
 	@RequestMapping(value = "/product/save", method = RequestMethod.POST)
-	public @ResponseBody Product createProduct(@ModelAttribute Product product, ModelMap model) {
+	public @ResponseBody Product createProduct(@ModelAttribute Product product) {
 		this.productService.addProduct(product);
 		return product;
 	}
 
 	@RequestMapping(value = "/product/delete", method = RequestMethod.GET)
-	public @ResponseBody boolean deleteProduct(@ModelAttribute Product product, ModelMap model) {
+	public @ResponseBody boolean deleteProduct(@ModelAttribute Product product) {
 		productService.deleteProduct(product);
 		return true;
 	}
 
 	@RequestMapping(value = "/product/update", method = RequestMethod.POST)
-	public @ResponseBody boolean updateProduct(@ModelAttribute Product product, ModelMap model) {
+	public @ResponseBody boolean updateProduct(@ModelAttribute Product product) {
 		productService.updateProduct(product);
 		return true;
 	}
